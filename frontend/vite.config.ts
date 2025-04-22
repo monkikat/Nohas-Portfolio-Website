92128e1b9a2bb97bda1bb9e6f1d39099e94b1a9f
Noha Haneen <nohahaneen@Nohas-MacBook-Pro.local>
2025-04-22 00:54:18 -0500

    added base for vercel deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { cloudflare } from '@cloudflare/vite-plugin'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +9,5 @@ export default defineConfig({
     tailwindcss(),
     react()
   ],
+  base: '/'
 })
